8e8983ce263887cf80d12d1fd9ebaf438930dbf9
ZedoMak <zerihunmekonen55@gmail.com>
2025-07-21 15:20:21 +0300

    2-customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 import {Button} from '@/components/ui/button'
+import {Input} from '@/components/ui/input'
+import {Textarea} from '@/components/ui/textarea'
+import {Progress} from '@/components/ui/progress'
+import { Checkbox } from '@/components/ui/checkbox'
 
 export default function Home(){
   return (
-    <div>
-      <p className='text-rose-500 text-4xl'>Hello world</p>
+
+    
+    <div className='flex flex-col gap-y-4 ml-5'>
+      <div className='p-4'></div>
+      <div>
+        <Button variant={'elevated'}>
+          I am A button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder='I am an Input' />
+      </div>
+
+      <div>
+        <Progress value={50}/>
+      </div>
+
+      <div>
+        <Textarea  placeholder='i am a text area'></Textarea>
+      </div>
+
+      <div>
+        <Checkbox/>
+      </div>
     </div>
   );
 };
